6b06c8dca0850b01fb700f96538ddd33eaf30b49
benkwok <benedictkwok@yahoo.com>
2019-11-19 15:16:50 -0800

    Add files via upload

---
+++ ck_blacklist.py
@@ -0,0 +1,72 @@
+import urllib3
+import csv
+from scapy.all import *
+from scapy.config import conf
+
+conf.use_pcap = True
+ips = []
+str1 = ''
+pcap = rdpcap("capture.cap")
+link = "https://isc.sans.edu/block.txt"
+mylines = []
+iplist=[]
+newblocklist=[]
+newcaplist=[]
+def fatch(link):
+	http = urllib3.PoolManager()
+	response = http.request('GET', link)
+	data = response.data.decode('utf-8')
+	assert isinstance(data, object)
+	with open('data3.txt','w') as f:
+		f.write(data)
+
+def remove_sign():
+	with open('data3.txt', 'r') as input_file:
+		with open('data4.txt', 'w') as output_file:
+			line: str
+			for line in input_file:
+				if (line[0] != "#"):
+					output_file.write(line)
+def blacklist():
+	with open('data4.txt', 'r') as file:
+		reader: object = csv.DictReader(file, delimiter='\t')
+		for row in reader:
+			iplist.append(row['Start'])
+	return iplist
+
+def remove_zero (iplist):
+	for row in iplist:
+			i = '{}.'.format(row.rsplit('.',1)[0])
+			newblocklist.append(i)
+	return newblocklist
+
+def remove_last_oct (ips):
+	for row in ips:
+			i = '{}.'.format(row.rsplit('.', 1)[0])
+			newcaplist.append(i)
+	return newcaplist
+
+def capture (pcap):
+	for pkt in pcap:
+		if (pkt.haslayer(IP)):
+			str1 = str(pkt[IP].src)
+			if str1 not in ips:
+				ips.append(str1)
+	return ips
+
+def main():
+	fatch(link)
+	remove_sign()
+	blacklist()
+	remove_zero(iplist)
+	capture(pcap)
+	remove_last_oct(ips)
+	newcaplist.append('89.248.174.')                # just to see the lambda work, append a blocked ip
+	print('Black List IPs: ',list(newblocklist))    # you can append any new block ip by x.y.z.
+	print('Captured IPs: ',list(newcaplist))
+	print ('Alert: Being Attack By: ', list(filter(lambda x: x in newcaplist,newblocklist)))
+
+if __name__ == "__main__":
+	main()
+
+
